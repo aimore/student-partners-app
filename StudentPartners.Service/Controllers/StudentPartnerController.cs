c5e8eee435f4455c69b5605312c899d275b99ceb
Pierce Boggan <pierceboggan14@gmail.com>
2016-08-09 17:27:33 -0400

    [Service] Cleaning up comments and references.

--- StudentPartners.Service/Controllers/StudentPartnerController.cs
+++ StudentPartners.Service/Controllers/StudentPartnerController.cs
@@ -3,10 +3,12 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.OData;
-using Microsoft.Azure.Mobile.Server;
+
 using StudentPartners.Service.DataObjects;
-using StudentPartners.Service.Models;
 using StudentPartners.Service.Helpers;
+using StudentPartners.Service.Models;
+
+using Microsoft.Azure.Mobile.Server;
 
 namespace StudentPartners.Service.Controllers
 {
@@ -31,21 +33,18 @@ namespace StudentPartners.Service.Controllers
         {
             return Lookup(id);
         }
-
-        // PATCH tables/StudentPartner/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        
         public Task<StudentPartner> PatchStudentPartner(string id, Delta<StudentPartner> patch)
         {
              return UpdateAsync(id, patch);
         }
-
-        // POST tables/StudentPartner
+        
         public async Task<IHttpActionResult> PostStudentPartner(StudentPartner item)
         {
             StudentPartner current = await InsertAsync(item);
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
-
-        // DELETE tables/StudentPartner/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        
         public Task DeleteStudentPartner(string id)
         {
              return DeleteAsync(id);
