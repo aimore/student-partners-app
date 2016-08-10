cf878d4f762dfffd584d470a8377b23a20e9123c
Pierce Boggan <pierceboggan14@gmail.com>
2016-08-10 11:19:37 -0400

    [App] Added scaffolding for login flow.

---
+++ StudentPartners/StudentPartners/ViewModels/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace StudentPartners.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        Command loginCommand;
+        public Command LoginCommand
+        {
+            get { return loginCommand ?? (loginCommand = new Command(async () => await ExecuteLoginCommandAsync())); }
+        }
+
+        async Task ExecuteLoginCommandAsync()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                // Log the user in!
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"ERROR: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
