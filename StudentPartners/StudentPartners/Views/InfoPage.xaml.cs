02ecc914d442b4ce55ba1a66a82861d393a989b6
wasserglas <ro.tobias@t-online.de>
2016-08-12 13:02:33 +0200

    [App] moved the edit button to the top corner of the profile page and added a Info page

---
+++ StudentPartners/StudentPartners/Views/InfoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StudentPartners
+{
+	public partial class InfoPage : ContentPage
+	{
+		public InfoPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
