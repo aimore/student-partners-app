0a90c1ac09cc712aede7662df31b80b8d660bca1
Pierce Boggan <pierce.boggan@xamarin.com>
2016-08-30 17:53:30 -0400

    Use TextCell for now, until student partners can upload their own photos.

--- StudentPartners/StudentPartners/Models/StudentPartner.cs
+++ StudentPartners/StudentPartners/Models/StudentPartner.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using AppServiceHelpers.Models;
+﻿using AppServiceHelpers.Models;
 using Newtonsoft.Json;
 
 namespace StudentPartners.Models
@@ -18,5 +12,11 @@ namespace StudentPartners.Models
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public Address Address { get; set; }
+
+		[JsonIgnore]
+		public string Name
+		{
+			get { return $"{FirstName} {LastName}"; }
+		}
     }
 }
